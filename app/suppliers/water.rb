ed8c35e9b008ea35a5d29e6b5734f8e046aa0683
Foysal Ahamed <cfaion341@gmail.com>
2015-07-30 19:42:03 +0200

    now using net/http to make http calls for scraping instead of using mechanize

--- app/suppliers/water.rb
+++ app/suppliers/water.rb
@@ -1,11 +1,6 @@
 module Suppliers
 	class Water
 		def initialize
-			@scraper = Mechanize.new { |agent|
-				agent.user_agent_alias = 'Mac Safari'
-				agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-			}
-
 			# Maintaining a list of available sites helps extending the scraper in the future
 			@supported_sites = ['affinity', 'thameswater']
 		end
@@ -16,13 +11,6 @@ module Suppliers
 
 			# Uncomment the timers to track the time needed for 2 different approaches
 			# puts Time.new.inspect
-=begin
-			if (querySite('affinity', postcode) == true)
-				@name = 'Affinity'
-			elsif (querySite('thameswater', postcode) == true)
-				@name = 'ThamesWater'
-			end
-=end
 			suppliers = Parallel.map(@supported_sites, :in_processes => @supported_sites.size) do |site|
 				if (querySite(site, postcode) == true)
 					@name = site
@@ -40,20 +28,27 @@ module Suppliers
 		private
 			# build the config for different sites
 			# this will allow us to extend and add more sites to scrape from
-			def getConfig (site)
+			def getConfig (site, postcode)
 				config = {}
 				if (site == 'affinity')
-					config[:url] = 'https://www.affinitywater.co.uk/our-supply-area-moving-home.aspx'
-					config[:form_id] = 'form1'
-					config[:field_name] = 'template$txtPostcodePanelSearch'
-					config[:submit_button_id] = 'template_btnPostcodePanelSearch'
+					config[:url] = 'https://www.affinitywater.co.uk/index.aspx?pg=79'
+					config[:fields] = {
+						'__VIEWSTATE' => '/wEPDwULLTE0MDkxNzYwNDNkGAIFDnRlbXBsYXRlJGN0bDIwDw9kBQ9PdXIgc3VwcGx5IGFyZWFkBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAQUSdGVtcGxhdGUkdHZDYXRMaXN0ie3aLRi1QlY9ITcusAySBuvkJNrFB0PpH7rBoPzz2s0=',
+						'__EVENTTARGET' => '',
+						'__EVENTARGUMENT' => '',
+						'__EVENTVALIDATION' => '/wEWBwLi4ZD9CgKFsLXuBwKo8smlAwLa3NCxBgKEs/GmAQLB34CeDwKw2NPZDSUa3TGeKEmzS9HyeEvobVjAkbaCIzNnZ/1Wc+/lB5qX',
+						'template$txtSearch' => 'Search this website',
+						'template$btnPostcodePanelSearch' => 'Search',
+						'template$txtPostcodePanelSearch' => postcode
+					}
 					config[:result_selector] = '#template_pnl_area_results'
 					config[:positive_result] = 'Your postcode appears on our database'
 				elsif (site == 'thameswater')
-					config[:url] = 'https://www.thameswater.co.uk/your-account/605.htm'
-					config[:form_id] = 'watersupplyform'
-					config[:field_name] = 'postcode1'
-					config[:submit_button_id] = 'submit'
+					config[:url] = 'https://secure.thameswater.co.uk/dynamic/cps/rde/xchg/corp/hs.xsl/Thames_Water_Supply.xml'
+					config[:fields] = {
+						'postcode1' => postcode,
+						'post_code' => postcode
+					}
 					config[:result_selector] = '.page-header'
 					config[:positive_result] = 'Your property is in our supply area'
 				end
@@ -67,22 +62,19 @@ module Suppliers
 					return nil
 				end
 
-				config = getConfig(site)
-				@scraper.get(config[:url]) do |page|
-					form = page.form_with(:id => config[:form_id])
-					
-					if (form.nil?)
-						return false
+				config = getConfig(site, postcode)
+				response = Net::HTTP.post_form(URI(config[:url]), config[:fields])
+
+				# for thameswater, the post call redirects to another page which is why we need this
+				case response
+					when Net::HTTPRedirection then
+						response = Net::HTTP.get_response(URI(response['location']))
 					end
 
-					form.field_with(:name => config[:field_name]).value = postcode
-					submit_button = form.button_with(:id => config[:submit_button_id])
-					
-					result_page = @scraper.submit(form, submit_button)
-					result_content = result_page.search(config[:result_selector]).first.text
+				result_page = Nokogiri::HTML(response.body)
+				result = result_page.search(config[:result_selector]).first
 
-					return result_content.match(config[:positive_result]) ? true : false;
-				end
+				return (!result.nil? && result.text.match(config[:positive_result])) ? true : false
 			end
 	end
 end
